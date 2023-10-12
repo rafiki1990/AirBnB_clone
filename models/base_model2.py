28bc9ca92b8e02cc776d49d8c2c689d974bc427f
macbook <macbook@macbooks-MacBook-Air.local>
2023-10-12 21:09:21 +0200

    test_base_model_dict.py

--- models/base_model2.py
+++ models/base_model2.py
@@ -4,11 +4,10 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-     if kwargs != []:
+        if kwargs != []:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        #Convert created_at and updated_at in datatime object.
                         value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
             if 'id' not in kwargs:
@@ -17,18 +16,18 @@ class BaseModel:
                 self.created_at = datetime.now()
             if 'updated_at' not in kwargs:
                 self.updated_at = datetime.now()
-            else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     def __str__(self):
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-    
+
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        
+
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         # Convert created_At and upated_at
@@ -36,4 +35,3 @@ class BaseModel:
         new_dict['updated_at'] = self.updated_at.isoformat()
 
         return new_dict
-
