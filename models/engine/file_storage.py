f2424da82593149186a6d35e9c1732d1da7f60e9
Rafiki Blaise <rafikiblaise@gmail.com>
2023-10-15 00:03:07 -0700

    test_save_reload_base_model.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        data = {}
+        for key, obj in self.__objects.items():
+            data[key] = obj.to_dict()
+        
+        with open(self.__file_path, 'w') as file:
+            json.dump(data, file)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    cls = models[class_name]
+                    self.__objects[key] = cls(**value)
+        except FileNotFoundError:
+            pass
+
