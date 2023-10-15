b486f57eab77820f0408b4cf97b40d1d3a30bba9
Rafiki Blaise <rafikiblaise@gmail.com>
2023-10-15 04:47:31 -0700

    console.py

--- console.py
+++ console.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb) '
-    def do_help(self, args):
-        pass
-    def do_quit(self, args):
-    
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
         return True
 
-    def do_EOF(self, args):
-
-        print()
+    def do_EOF(self, arg):
+        """Exit the program"""
         return True
-
-    def emptyline(self):
-        pass
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    console = HBNBCommand()
+    console.cmdloop()
